d9153c737526c9ffb0cf6284bbaf36ce9e30e246
Chris Marks <cmarks@fullsail.com>
2017-05-23 11:12:12 -0400

    Cleaned up menu system a bit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string sceneName){
+		Debug.Log("Level load requested for: " + sceneName);
+		SceneManager.LoadScene(sceneName);
+	}
+
+	public void LoadNextLevel(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		print("Load next level");
+	}
+
+	public void Quit(){
+		Debug.Log("Quit Application");
+		Application.Quit();
+	}
+
+}
